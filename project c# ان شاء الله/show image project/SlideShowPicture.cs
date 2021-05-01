e4c60136a783f0608db1a26437782aa82d5a3a58
abdulrahmangamalahmedgaber@gmail.com <abdulrahmangamalahmedgaber@gmail.com>
2021-05-01 20:44:28 +0200

    c # project

---
+++ project c# ان شاء الله/show image project/SlideShowPicture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbdoGamal
+{
+    //Here is everything related to the third display system.
+    //I tried to apply the things to her studies at the oop and SW
+
+
+    class ShowSlidePicture 
+    {
+
+
+
+        public void ShowSlidePictureFun(int i, ListBox listOfNameOfPicture, PictureBox pictureBox1,List<string> listOfPathToPicture,out string statBarShowNameOfPicture)
+        {
+            try
+            {
+                
+
+                if (listOfPathToPicture.Count > 0&&i< listOfPathToPicture.Count)
+                {
+
+                 
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                  pictureBox1.Image = Image.FromFile(listOfPathToPicture[i]);
+
+                    
+                    
+                }
+                else if( listOfPathToPicture.Count==0)
+                {
+                    if(listOfNameOfPicture.Visible==true)
+                    MessageBox.Show("you do not add any picture ):...");
+
+                }
+                else
+                {
+                    MessageBox.Show("Errors ):...");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            statBarShowNameOfPicture = Path.GetFileName(listOfPathToPicture[i]);
+
+        }
+
+
+
+    }
+}
